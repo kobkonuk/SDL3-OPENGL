827eea5b93a2974a3b0d493a496d13780c5ce8dd
kobkonuk <jakobkobkob@proton.me>
2026-05-18 19:44:44 +0200

    added uniforms and flashing cube

---
+++ main.cpp
@@ -0,0 +1,236 @@
+#define  SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <GL/glew.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+typedef enum {
+	MENU,
+	PLAYING,
+	PAUSE,
+	GAME_OVER,
+	WIN,
+} GameState;
+
+static GameState game_state = PLAYING;
+static SDL_Window *window = NULL;
+static SDL_GLContext gl_context;
+static unsigned int vao;
+static unsigned int buffer;
+static unsigned int ibo;
+static unsigned int shader;
+
+static float r = 0.0f;
+static float increment = 0.05f;
+
+static int location;
+
+static void GLClearError() {
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static void GLCheckError() {
+	while (GLenum error = glGetError()) {
+		std::cout << "[OpenGL fail] ( " << error << " )" << std::endl;
+	}
+}
+
+struct ShaderProgramSource {
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath) {
+	std::ifstream stream(filepath);
+	
+	enum class ShaderType {
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+
+	ShaderType type = ShaderType::NONE;
+
+	while(getline(stream, line)) {
+		if (line.find("#shader") != std::string::npos) {
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else {
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result = 0;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		int length = 0;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader" << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+
+	int height = 1080;
+	int width = 1080;
+
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_Log("Couldn't initialize : %s ", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	
+	window = SDL_CreateWindow(
+		"red-lantern 3D",
+		width,
+		height,
+		SDL_WINDOW_OPENGL
+	);
+
+	if (window == NULL)
+		std::cout << "SDL_CreateWindow fail" << std::endl;
+
+	gl_context = SDL_GL_CreateContext(window);
+
+	if (SDL_GL_SetSwapInterval(-1) < 0)
+		SDL_GL_SetSwapInterval(1);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+		std::cout << "GLEW_NOT_OK" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[] = {
+	   -0.5f, -0.5f,
+		0.5f, -0.5f,
+		0.5f,  0.5f,
+
+       -0.5f,  0.5f,
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indices, GL_STATIC_DRAW);
+
+	ShaderProgramSource source = ParseShader("res/shaders/basic.shader");
+
+	shader = CreateShader(source.VertexSource, source.FragmentSource);
+	glUseProgram(shader);
+
+	location = glGetUniformLocation(shader, "u_Color");
+
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+	
+	if (game_state == MENU) {
+		std::cout << "hello cro" << std::endl;	
+	} 
+
+	else if (game_state == PLAYING) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		GLClearError();
+
+		glBindVertexArray(vao);
+
+		glUniform4f(location, r, 1.0f, 1.0f, 1.0f);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r < 0.0f)
+			increment = 0.05f;
+
+		r += increment;
+		
+		GLCheckError();
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+	
+	if (event->type == SDL_EVENT_QUIT) {
+		return SDL_APP_SUCCESS;
+	}
+
+	if (game_state == MENU &&
+		event->type == SDL_EVENT_KEY_DOWN &&
+		event->key.key == SDLK_RETURN) {
+		game_state = PLAYING;
+		SDL_Log("Game state = playing");
+	}
+
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+	//glDeleteProgram(shader);
+	SDL_Quit();
+}
